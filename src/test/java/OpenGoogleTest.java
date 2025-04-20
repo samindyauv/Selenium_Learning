3de1950ccc8c12f12f626d6a45c3bacb44bb75d2
Samindya Vass <samindyauv@gmail.com>
2025-04-20 21:21:27 +0530

    Initial commit

---
+++ src/test/java/OpenGoogleTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenGoogleTest {
+
+    @Test
+    public void googleTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.quit();
+    }
+}
